16a14f95efb45b3f49aec95c0f07b0f9d8f3af44
EclipseBringuer <118445079+EclipseBringuer@users.noreply.github.com>
2022-12-19 10:21:10 +0100

    Add files via upload

---
+++ GabrielRincónLópez/galeria.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Web sencilla de im&aacute;genes">
+	<meta name="keywords" content="HTML5, CSS3, JavaScript">
+  <link rel="stylesheet" href="EstilosCSS.css">
+	<title>Flores</title>
+
+	<style>
+		div{
+			background-color: greenyellow;
+			padding: 10px;
+		}
+	</style>
+
+
+</head>
+<body>
+
+
+  <h1>EJEMPLO EJERCICIO. Galer&iacute;as de im&aacute;genes</h1>
+  <h2>Una selecci&oacute;n de flores.</h2>
+
+  <p class="l">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores labore adipisci excepturi facilis! Eligendi ex odio minima. Libero, aspernatur accusamus explicabo quibusdam quod, officia sunt eum, nihil commodi natus deserunt?<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias et suscipit doloribus optio fugiat iusto non officia quaerat possimus, aliquid, nisi ratione tenetur rem, harum tempore inventore illum. Hic, adipisci.</p>
+   
+ 
+      <img src="imagenes/1.jpg" alt="Flores de corte" width="200" height="200">
+    
+      Foto 1: Flores de corte   
+     
+    
+      <img src="imagenes/2.jpg" alt="Mirabilis Jalapa" width="200" height="200">
+     
+      Foto 2: Flor de Mirabilis Jalapa
+     
+    
+      <img src="imagenes/3.jpg" alt="Strelitzia" width="200" height="200">
+     
+     Foto3: Strelitzia. Flores de las aves.
+    
+    
+       <img src="imagenes/4.jpg" alt="Flor del opio" width="200" height="200">
+      
+      Foto 4: Flor del opio.
+      
+   
+       <img src="imagenes/5.jpg" alt="Rosas" width="200" height="200">
+     
+      Foto 5: Rosas
+     
+    
+      <img src="imagenes/6.jpg" alt="Pensamiento" width="200" height="200">
+     
+     Foto 6: Pensamiento
+    
+     <p class="l">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet doloribus debitis facere, ullam architecto pariatur tempora! Temporibus hic, quisquam omnis ea id sed recusandae doloribus, repellendus quia culpa dolor cupiditate?<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias et suscipit doloribus optio fugiat iusto non officia quaerat possimus, aliquid, nisi ratione tenetur rem, harum tempore inventore illum. Hic, adipisci.</p>   
+<p class="r">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta excepturi qui sapiente totam quisquam laboriosam odio possimus maiores. Odio, officiis saepe iure asperiores accusantium reprehenderit sapiente nulla repellat<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias et suscipit doloribus optio fugiat iusto non officia quaerat possimus, aliquid, nisi ratione tenetur rem, harum tempore inventore illum. Hic, adipisci.</p>
+<div>
+<h1 Style="color:red">
+  <?php
+  echo'Autor:(Gabriel Rincón López)';
+  ?>
+  </h1>
+ </div>
+</body>
+</html>
